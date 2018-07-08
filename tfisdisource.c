ddd216a594e1efa38949c206f43ef562da3d461a
Andy Chang <andy.chang@tfidm.com>
2018-07-08 21:07:56 +0800

    implement flow with using gstbasesrc

--- tfisdisource.c
+++ tfisdisource.c
@@ -2,8 +2,10 @@
 #include "config.h"
 #endif
 #include "tfisdisource.h"
+#include "gstmyfilter.h"
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 GST_DEBUG_CATEGORY_STATIC (tfi_sdi_src_debug);
 #define GST_CAT_DEFAULT tfi_sdi_src_debug
@@ -27,57 +29,42 @@ GST_STATIC_PAD_TEMPLATE ("audio%u",
 
 #define _do_init \
     GST_DEBUG_CATEGORY_INIT (tfi_sdi_src_debug, "tfi_sdi_src", 0, "TFI SDI source plugin");
-G_DEFINE_TYPE_WITH_CODE (TfiSdiSrc, tfi_sdi_src, GST_TYPE_BASE_SRC, _do_init);
+G_DEFINE_TYPE_WITH_CODE (TfiSdiSrc, tfi_sdi_src, GST_TYPE_ELEMENT, _do_init);
 
-static void gst_sdi_src_set_property (GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec);
-static void gst_sdi_src_get_property (GObject *object, guint prop_id, GValue *value, GParamSpec *pspec);
-static GstCaps *tfi_sdi_src_src_fixate (GstBaseSrc *bsrc, GstCaps *caps);
-static void tfi_sdi_src_get_times (GstBaseSrc *basesrc, GstBuffer *buffer, GstClockTime *start, GstClockTime *end);
-static gboolean tfi_sdi_src_decide_allocation (GstBaseSrc *bsrc, GstQuery *query);
-static GstFlowReturn tfi_sdi_src_fill (GstBaseSrc *bsrc, guint64 offset, guint length, GstBuffer * ret);
-static gboolean tfi_sdi_src_start (GstBaseSrc *basesrc);
-static gboolean tfi_sdi_src_stop (GstBaseSrc *basesrc);
+static void gst_sdi_src_set_property (
+        GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec);
+static void gst_sdi_src_get_property (
+        GObject *object, guint prop_id, GValue *value, GParamSpec *pspec);
+static GstStateChangeReturn gst_base_src_change_state (
+        GstElement * element, GstStateChange transition);
+static void gst_base_src_loop (GstPad * pad);
 
 static void
 tfi_sdi_src_class_init (TfiSdiSrcClass * klass)
 {
     GObjectClass *gobject_class;
     GstElementClass *gstelement_class;
-    GstBaseSrcClass *gstbasesrc_class;
 
     gobject_class = (GObjectClass *) klass;
-    gstelement_class = (GstElementClass *) klass;
-    gstbasesrc_class = (GstBaseSrcClass *) klass;
-
     gobject_class->set_property = gst_sdi_src_set_property;
     gobject_class->get_property = gst_sdi_src_get_property;
+
+    gstelement_class = (GstElementClass *) klass;
+    gstelement_class->change_state = GST_DEBUG_FUNCPTR (gst_base_src_change_state);
 
     gst_element_class_set_static_metadata (gstelement_class,
             "TFI SDI source", "Source/Video",
             "Integrate with Blackmagic SDI card", "Andy Chang <andy.chang@tfidm.com>");
 
-    gst_element_class_add_static_pad_template (gstelement_class, &video_src_template);
-    //gst_element_class_add_static_pad_template (gstelement_class, &audio_src_template);
-
-    gstbasesrc_class->fixate = tfi_sdi_src_src_fixate;
-    gstbasesrc_class->get_times = tfi_sdi_src_get_times;
-    gstbasesrc_class->start = tfi_sdi_src_start;
-    gstbasesrc_class->stop = tfi_sdi_src_stop;
-    gstbasesrc_class->decide_allocation = tfi_sdi_src_decide_allocation;
-    gstbasesrc_class->fill = tfi_sdi_src_fill;
+    //gst_element_class_add_static_pad_template (gstelement_class, &video_src_template);
 }
 
 static void
 tfi_sdi_src_init (TfiSdiSrc *src)
 {
-    gst_base_src_set_format (GST_BASE_SRC (src), GST_FORMAT_TIME);
-    gst_base_src_set_live (GST_BASE_SRC (src), DEFAULT_IS_LIVE);
-}
-
-static GstCaps *
-tfi_sdi_src_src_fixate (GstBaseSrc *bsrc, GstCaps *caps)
-{
-    return NULL;
+    src->stream_start_pending = TRUE;
+    src->srcpad = gst_pad_new_from_static_template (&video_src_template, "src");
+    gst_element_add_pad (GST_ELEMENT (src), src->srcpad);
 }
 
 static void
@@ -90,59 +77,72 @@ gst_sdi_src_get_property (GObject *object, guint prop_id, GValue *value, GParamS
 {
 }
 
-static gboolean
-tfi_sdi_src_decide_allocation (GstBaseSrc *bsrc, GstQuery *query)
+static GstStateChangeReturn
+gst_base_src_change_state (GstElement * element, GstStateChange transition)
 {
-    return TRUE;
+    TfiSdiSrc *src;
+    src = TFI_SDI_SRC (element);
+
+    g_print("change state\n");
+
+    switch (transition) {
+        case GST_STATE_CHANGE_NULL_TO_READY:
+            g_print("GST_STATE_CHANGE_NULL_TO_READY\n");
+            break;
+        case GST_STATE_CHANGE_READY_TO_PAUSED:
+            g_print("GST_STATE_CHANGE_READY_TO_PAUSED\n");
+            break;
+        case GST_STATE_CHANGE_PAUSED_TO_PLAYING:
+            g_print("GST_STATE_CHANGE_PAUSED_TO_PLAYING\n");
+            gst_pad_start_task (
+                    src->srcpad, (GstTaskFunction) gst_base_src_loop,
+                    src->srcpad, NULL);
+            break;
+        default:
+            break;
+    }
+    //return GST_STATE_CHANGE_SUCCESS;
+    return GST_STATE_CHANGE_NO_PREROLL;
 }
 
 static void
-tfi_sdi_src_get_times (GstBaseSrc *basesrc, GstBuffer *buffer, GstClockTime *start, GstClockTime *end)
+gst_base_src_loop (GstPad * pad)
 {
-    /* for live sources, sync on the timestamp of the buffer */
-    if (gst_base_src_is_live (basesrc)) {
-        GstClockTime timestamp = GST_BUFFER_PTS (buffer);
+    TfiSdiSrc *src;
+    GstBuffer *buf = NULL;
+    GstMemory *memory;
 
-        //g_print("is live:%lld\n", timestamp);
-        if (GST_CLOCK_TIME_IS_VALID (timestamp)) {
-            /* get duration to calculate end time */
-            GstClockTime duration = GST_BUFFER_DURATION (buffer);
+    src = TFI_SDI_SRC (GST_OBJECT_PARENT (pad));
 
-            if (GST_CLOCK_TIME_IS_VALID (duration)) {
-                *end = timestamp + duration;
-            }
-            *start = timestamp;
-        }
-    } else {
-        g_print("is file\n");
-        *start = -1;
-        *end = -1;
+    if (src->stream_start_pending == TRUE)
+    {
+        gchar *stream_id;
+        GstEvent *event;
+
+        stream_id = gst_pad_create_stream_id (src->srcpad, GST_ELEMENT_CAST (src), NULL);
+        event = gst_event_new_stream_start (stream_id);
+        gst_event_set_group_id (event, gst_util_group_id_next ());
+
+        gst_pad_push_event (src->srcpad, event);
+        g_free (stream_id);
+        src->stream_start_pending = FALSE;
     }
+
+    g_print("loop\n");
+    buf = gst_buffer_new ();
+    memory = gst_allocator_alloc (NULL, 1234, NULL);
+    gst_buffer_insert_memory (buf, -1, memory);
+    gst_pad_push (pad, buf);
+    sleep(1);
 }
 
-static GstFlowReturn
-tfi_sdi_src_fill (GstBaseSrc *bsrc, guint64 offset, guint length, GstBuffer *ret)
-{
-    g_print("buffersize: %ld,%d,%p\n", offset, length, ret);
-    return GST_FLOW_OK;
-}
-
-static gboolean
-tfi_sdi_src_start (GstBaseSrc *basesrc)
-{
-    return TRUE;
-}
-
-static gboolean
-tfi_sdi_src_stop (GstBaseSrc *basesrc)
-{
-    return TRUE;
-}
 
 static gboolean
 plugin_init (GstPlugin *plugin)
 {
-    return gst_element_register (plugin, "tfi_sdi_src", GST_RANK_NONE, GST_TYPE_TFI_SDI_SRC);
+    gst_element_register (plugin, "tfi_sdi_src", GST_RANK_NONE, GST_TYPE_TFI_SDI_SRC);
+    gst_element_register (plugin, "myfilter", GST_RANK_NONE, GST_TYPE_MYFILTER);
+    return TRUE;
 }
 
 GST_PLUGIN_DEFINE (
--- tfisdisource.h
+++ tfisdisource.h
@@ -2,21 +2,23 @@
 #define __GST_TFI_SDI_SRC_H__
 
 #include <gst/gst.h>
-#include <gst/base/gstbasesrc.h>
 
 G_BEGIN_DECLS
 
 #define GST_TYPE_TFI_SDI_SRC (tfi_sdi_src_get_type())
+#define TFI_SDI_SRC(obj)     (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_TFI_SDI_SRC,TfiSdiSrc))
 
 typedef struct _TfiSdiSrc TfiSdiSrc;
 typedef struct _TfiSdiSrcClass TfiSdiSrcClass;
 
 struct _TfiSdiSrc {
-  GstBaseSrc element;
+  GstElement element;
+  GstPad *srcpad;
+  gboolean stream_start_pending;
 };
 
 struct _TfiSdiSrcClass {
-  GstBaseSrcClass parent_class;
+  GstElementClass parent_class;
 };
 
 GType tfi_sdi_src_get_type (void);
